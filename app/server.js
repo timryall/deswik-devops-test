b80484d94d5fb0f86cb089f33a4eb6a7998afbe9
Tim Ryall <88timryall@gmail.com>
2026-03-12 16:59:04 +1000

    feat: example app change

--- app/server.js
+++ app/server.js
@@ -4,10 +4,11 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Good Morning!\n');
+  res.write('Good Afternoon!\n');
   res.end('Hello Deswik!\n');
 });
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Hello Deswik!!!`);
 });
