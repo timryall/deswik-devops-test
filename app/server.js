211a46a7a353854e6fd17c539a921fffb555faa9
Tim Ryall <88timryall@gmail.com>
2026-03-12 12:36:28 +1000

    feat: example changes to app config

--- app/server.js
+++ app/server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Good Morning!\n');
   res.end('Hello Deswik!\n');
 });
 
